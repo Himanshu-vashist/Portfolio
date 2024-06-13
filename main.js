deeca9c7bad877a2808c769ee606bbd64ffc05c6
himanshu <imhimanshuvashist@gmail.com>
2024-06-13 18:02:04 +0530

    Update portfolio website with new images and modifications

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ var typed = new Typed(".text",{
     backDelay : 1000,
     loop : true
 });
+
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the default form submission behavior
+
+    // Capture the form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    // Log the form data to the console
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Subject:', subject);
+    console.log('Message:', message)
+});
